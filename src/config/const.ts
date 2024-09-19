73adb315a5b34f469106a05467c0276fe66f3ae0
lryan8959 <lryan8959@gmail.com>
2024-09-18 17:27:52 -0700

    updated backend url

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,6 +1,6 @@
 // export const baseUrl = "https://localhost:3120";
 // export const baseUrl = "https://api.m59media.com";
-export const baseUrl = "http://194.163.45.154:5000"; 
+export const baseUrl = "https://nestjs-replituser51575.replit.app"; 
 
 
 // export const baseUrl = "http://localhost:3000";
